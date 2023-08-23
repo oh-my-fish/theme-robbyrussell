9d260624ee9d6e2209a2ed38b0862c9e91b3d75e
Matt Keeter <matt@oxide.computer>
2023-08-23 11:13:00 -0400

    Add --no-optional-locks to `git status`

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -13,7 +13,7 @@ function _is_git_dirty
   if [ "$theme_display_git_untracked" = 'no' -o "$show_untracked" = 'false' ]
     set untracked '--untracked-files=no'
   end
-  echo (command git status -s --ignore-submodules=dirty $untracked 2> /dev/null)
+  echo (command git status -s --no-optional-locks --ignore-submodules=dirty $untracked 2> /dev/null)
 end
 
 function fish_prompt
